ab4a3b266fc53772c3dd5ee7121c42202495336f
DanielSoltysiak <daniel.soltysiak13@gmail.com>
2020-11-02 11:10:17 +0100

    Dodałem "tryb nocny" i tło

--- Lab2/assets/main.js
+++ Lab2/assets/main.js
@@ -1,3 +1,34 @@
+function chceckingTime(){
+    var today = new Date();
+    var hour = today.getHours();
+    if(hour>=18 || hour<=6) document.getElementById("lightSwitch").checked = "true";
+}
+function turningLight(){
+    if(document.getElementById("lightSwitch").checked){
+        document.body.classList.add("nightMode");
+        document.getElementById("lightSwitchLabel").innerHTML = "Wyłącz tryb nocny";
+        var ele = document.getElementsByClassName("card");
+        for(i=0; i<ele.length; i++){
+            ele[i].classList.add("bg-secondary")
+        }
+        var ele = document.getElementsByClassName("list-group-item");
+        for(i=0; i<ele.length; i++){
+            ele[i].classList.add("list-group-item-dark")
+        }
+    } 
+    else{
+        document.body.classList.remove("nightMode")
+        document.getElementById("lightSwitchLabel").innerHTML = "Włącz tryb nocny"
+        var ele = document.getElementsByClassName("card");
+        for(i=0; i<ele.length; i++){
+            ele[i].classList.remove("bg-secondary")
+        }
+        var ele = document.getElementsByClassName("list-group-item");
+        for(i=0; i<ele.length; i++){
+            ele[i].classList.remove("list-group-item-dark")
+        }
+    }
+}
 function clockCheck(clockVar){
     if(clockVar<10) clockVar="0"+clockVar;
     return clockVar;
