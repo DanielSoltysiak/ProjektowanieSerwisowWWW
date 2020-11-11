30cf2f3eb1fbd184e3b0803b38ea011d4ef66c2b
DanielSoltysiak <daniel.soltysiak13@gmail.com>
2020-11-11 15:10:14 +0100

    Zadanie wykonane

--- Lab3/assets/main.js
+++ Lab3/assets/main.js
@@ -1,3 +1,16 @@
+function classesAdd(){
+    document.querySelectorAll("h2").forEach(function(el){el.classList.add('card-title')});
+    Array.from(document.getElementsByClassName("card")).forEach(function(el){el.classList.add("mb-2")});
+    Array.from(document.getElementsByClassName("list-group-item")).forEach(function(el){el.classList.add("list-group-item-action")});
+}
+function dragPhotoEvent(){
+    document.getElementById("cardDrag").appendChild(document.createTextNode("Zostaw to!"));
+    document.getElementById("backgroundPhoto").removeEventListener("drag",dragPhotoEvent);
+}
+function addColorListeners(){
+    document.addEventListener("keypress", backgroundColorBlue);
+    document.addEventListener("keyup",backgroundColorGrey);
+}
 function backgroundColorBlue(){
     document.body.style.background = "LightBlue";
     document.removeEventListener("keypress", backgroundColorBlue);
@@ -9,10 +22,20 @@ function backgroundColorGrey(){
 }
 function backgroundColorWhite(){
     document.body.style.background = "white";
-    document.addEventListener("keydown", addListeners);
 }
-function addListeners(){
-    document.removeEventListener("keydown", addListeners);
-    document.addEventListener("keypress", backgroundColorBlue);
-    document.addEventListener("keyup",backgroundColorGrey);
+function scrollEvent(){
+    document.body.style.color = "blue";
 }
+function selectChange(){
+    document.getElementById("fruitSelectOutput").innerHTML = "Twój ulubiony owoc to: "+document.getElementById("fruitSelect").value;
+}
+function inputEvent(){
+    document.getElementById("textOutput").innerHTML = document.getElementById("textInput").value;
+}
+function copyEvent(){
+    document.getElementById("copyWarning").innerHTML = "Pamiętaj że plagiat jest nielegalny!";
+    document.getElementById("cardCopy").classList.add("warning");
+}
+document.addEventListener("wheel", scrollEvent);
+document.addEventListener("input",scrollEvent);
+document.addEventListener("copy",copyEvent);
